1ceb7b3e99f82b55eebd77ffbf03d961913907c9
Lex Draven <Lexman2@ya.ru>
2016-04-23 10:56:01 +0500

    +1

--- src/StartClass.java
+++ src/StartClass.java
@@ -2,25 +2,33 @@
 public class StartClass {
 
     public static void main(String[] args) {
-        Integer [] mass = new Integer[10000];
-        for (int i=0; i<mass.length;i++){
-            mass[i] = (int)(Math.random()*10000);
-        }
+        Integer [] mass = getArray(20000);
         SortMachine sortMachine = new SortMachine(mass);
         sortMachine.setShowTimeResults(true);
         sortMachine.print();
         sortMachine.mergeSort();
         sortMachine.print();
-        sortMachine.setNewArray(mass);
-        sortMachine.insertionSort();
+        sortMachine.setNewArray(getArray(20000));
         sortMachine.print();
-        sortMachine.setNewArray(mass);
-        sortMachine.selectedSort();
-        sortMachine.print();
-        sortMachine.setNewArray(mass);
         sortMachine.bubbleSort();
         sortMachine.print();
+        sortMachine.setNewArray(getArray(20000));
+        sortMachine.print();
+        sortMachine.selectedSort();
+        sortMachine.print();
+        sortMachine.setNewArray(getArray(20000));
+        sortMachine.print();
+        sortMachine.insertionSort();
+        sortMachine.print();
 
 
+    }
+
+    public static Integer [] getArray (int size){
+        Integer [] mass = new Integer[size];
+        for (int i=0; i<mass.length;i++){
+            mass[i] = (int)(Math.random()*(size*3));
+        }
+        return mass;
     }
 }
