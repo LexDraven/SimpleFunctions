b9b22dcd07c36fe2c80bfca58a8555b032bb8f46
LexDraven <Lexman2@ya.ru>
2017-03-29 20:29:30 +0500

    1

--- src/main/java/Net/SlemmaOnlineChecker.java
+++ src/main/java/Net/SlemmaOnlineChecker.java
@@ -6,7 +6,7 @@ import Simple.URLChecker;
 public class SlemmaOnlineChecker {
 
     public static void main(String[] args) {
-        MailSender mailSender = new MailSender("a.kotomin@slemma.com","Q123#@!w");
+        MailSender mailSender = new MailSender("","");
         mailSender.sendEmail("farofwell@gmail.com", "Checker is running","The check is on the line!");
         int code = 200;
         URLChecker checker = new URLChecker();
@@ -20,7 +20,7 @@ public class SlemmaOnlineChecker {
                 break;
             }
         }
-        //MailSender mailSender = new MailSender("a.kotomin@slemma.com","Q123#@!w");
-        mailSender.sendEmail("farofwell@gmail.com", "The release is DOWN!","The response from slemma.com is not 200! And the answer code is "+code);
+        //MailSender mailSender = new MailSender("","");
+        mailSender.sendEmail("login", "The release is DOWN!","The response from slemma.com is not 200! And the answer code is "+code);
     }
 }
